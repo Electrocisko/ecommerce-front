c2e23adbff9f95db900ea23ab76b535982a9b66c
Francisco Zuidwijk <electrocisko@gmail.com>
2024-07-02 18:35:25 -0300

    styles in navbar components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,33 @@
-import style from '../scss/modules/navbar.module.scss'
+import style from '../scss/modules/navbar.module.scss';
+import { LuShoppingCart } from "react-icons/lu";
+import { FaRegCircleUser } from "react-icons/fa6";
+import { FaAngleDown } from "react-icons/fa6";
+import { FaSearch } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <div className={style.title}>
-        <h1>NavBar</h1>
-    </div>
+  <nav>
+    <ul className={style.ul_list}>
+      <li className={style.logo}>SHOP.CO</li>
+
+    
+
+     <div className={style.center_links}>
+     <li className={style.dropdown}>Shop <FaAngleDown/></li>
+     <li>On Sale</li>
+      <li>New Arrivals</li>
+      <li>Brands</li>
+     </div>
+  
+      <li className={style.searchbar}>
+        <FaSearch/>
+        Search for products....</li>
+      <div className={style.right_links}>
+        <li className={style.icon}><LuShoppingCart /></li>
+        <li className={style.icon}><FaRegCircleUser/></li>
+      </div>
+    </ul>
+  </nav>
   )
 }
 
