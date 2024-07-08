63429dea4b7d79ced9d1d79847a6bb55d9aeffa5
Francisco Zuidwijk <electrocisko@gmail.com>
2024-07-08 15:19:06 -0300

    working on footer styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,9 @@
 import style from "../scss/modules/footer.module.scss";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaFacebook } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa6";
+
 
 const Footer = () => {
   return (
@@ -9,6 +14,17 @@ const Footer = () => {
         <p>
           We have clothes that suits your style and which you’re proud to wear. </p>
           <p>From women to men.</p>
+          <div className={style.socialnetworks}>
+            <a href="/" className={style.icon}><FaSquareXTwitter /></a>
+            <a href="/" className={style.icon} ><FaFacebook /></a>
+            <a href="/" className={style.icon} ><FaInstagram /></a>
+            <a href="/" className={style.icon} ><FaGithub /></a>
+        
+        
+         
+        
+
+          </div>
         </div>
      
       </div>
@@ -16,10 +32,10 @@ const Footer = () => {
       <div className={style.card}>
         <h3>COMPANY</h3>
         <div className={style.links}>
-        <a href="">About</a>
-        <a href="">Features</a>
-        <a href="">Works</a>
-        <a href="">Career</a>
+        <a href="/">About</a>
+        <a href="/">Features</a>
+        <a href="/">Works</a>
+        <a href="/">Career</a>
         </div>
       
       </div>
@@ -27,10 +43,10 @@ const Footer = () => {
       <div className={style.card}>
         <h3>HELP</h3>
         <div className={style.links}>
-        <a href="">About</a>
-        <a href="">Features</a>
-        <a href="">Works</a>
-        <a href="">Career</a>
+        <a href="/">About</a>
+        <a href="/">Features</a>
+        <a href="/">Works</a>
+        <a href="/">Career</a>
         </div>
       
       </div>
@@ -38,10 +54,10 @@ const Footer = () => {
       <div className={style.card}>
         <h3>FAQ</h3>
         <div className={style.links}>
-        <a href="">About</a>
-        <a href="">Features</a>
-        <a href="">Works</a>
-        <a href="">Career</a>
+        <a href="/">About</a>
+        <a href="/">Features</a>
+        <a href="/">Works</a>
+        <a href="/">Career</a>
         </div>
     
       </div>
@@ -49,10 +65,10 @@ const Footer = () => {
       <div className={style.card}>
         <h3>RESOURCES</h3>
         <div className={style.links}>
-        <a href="">About</a>
-        <a href="">Features</a>
-        <a href="">Works</a>
-        <a href="">Career</a>
+        <a href="/">About</a>
+        <a href="/">Features</a>
+        <a href="/">Works</a>
+        <a href="/">Career</a>
         </div>
       
       </div>
