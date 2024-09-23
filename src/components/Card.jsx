d92ac39178ae3291954d52854cd34d042a5a7ed0
Francisco Zuidwijk <electrocisko@gmail.com>
2024-09-23 15:01:13 -0300

    se agrego loading en imagenes de card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,26 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react";
 import style from "../scss/modules/card.module.scss";
 
 
 const Card = ({name, price, urlImage}) => {
+
+  const [loading, setLoading] = useState(true);
+
+  const handleLoad = () => {
+    setLoading(false)
+  }
+
+
   return (
     <div className={style.card}>
-        <img src={urlImage} alt={"Imagen de "+name} />
+         {loading && <div className={style.spinner}> <h2>Loading...</h2></div>}
+      <img 
+        src={urlImage} 
+        alt={"Imagen de " + name} 
+        onLoad={handleLoad} 
+        style={{ display: loading ? 'none' : 'block' }} 
+      />
         <h3>{name}</h3>
         <h4>{price}</h4>
     </div>
