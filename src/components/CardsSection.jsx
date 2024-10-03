f9a4336e75bff5690b539f5af3dca4ea55b70e21
Francisco Zuidwijk <electrocisko@gmail.com>
2024-10-03 17:26:14 -0300

    componentes

---
+++ src/components/CardsSection.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable react/prop-types */
+import { Link } from "react-router-dom";
+import style from "../scss/modules/cardssection.module.scss";
+import ButtonLight from "./smalls/ButtonLight";
+import Card from "./Card";
+import { urlServer } from "../data/endpoints.js";
+
+const CardsSection = ({ products, title }) => {
+  return (
+    <section className={style.section}>
+      <h2 className={style.h2}>{title}</h2>
+      <div className={style.card_container}>
+        {products.map((item) => (
+          <Link to={"/detail/" + item.product_id} key={item.product_id}>
+            <Card
+              name={item.name}
+              price={item.price}
+              urlImage={urlServer + "images/" + item.imageurl}
+            />
+          </Link>
+        ))}
+      </div>
+      <a href="/" className={style.button_center}>
+        <ButtonLight text={"View All"} />
+      </a>
+    </section>
+  );
+};
+
+export default CardsSection;
+
--- src/components/HomeMain.jsx
+++ src/components/HomeMain.jsx
@@ -1,81 +1,59 @@
 import Button from "./smalls/Button";
 import style from "../scss/modules/homemain.module.scss";
 import { useLoaderData } from "react-router-dom";
-import Card from "../components/Card";
-import {urlServer} from "../data/endpoints.js"
-import ButtonLight from "./smalls/ButtonLight";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import StyleSection from "./StyleSection.jsx";
-
+import CardsSection from "./CardsSection.jsx";
 
 const HomeMain = () => {
+  const { newProducts, topProducts } = useLoaderData();
+  const firstNewsProducts = newProducts.newProducts.slice(0, 4);
+  const firstTopProducts = topProducts.topsells.slice(0, 4);
 
-const { newProducts, topProducts } = useLoaderData();
- const firstNewsProducts = newProducts.newProducts.slice(0,4);
- const firstTopProducts = topProducts.topsells.slice(0, 4);
-  
   return (
     <>
-    <main className={style.container}>
-      <div>
-        <h1 className={style.title}>FIND CLOTHES THAT MATCHES YOUR STYLE</h1>
-        <p className={style.text}>
-          Browse through our diverse range of meticulously crafted garments,
-          designed to bring out your individuality and cater to your sense of
-          style.
-        </p>
-        <Link to="/shop"><Button text={"Shop Now"} /></Link>
-        
-        <div className={style.data_container}>
-          <div>
-            <h3 className={style.text_data}>200+</h3>
-            <p className={style.text_data_small}>International Brands</p>
-          </div>
-          <div>
-            <h3 className={style.text_data}>2,000+</h3>
-            <p className={style.text_data_small}>High-Quality Products</p>
-          </div>
-          <div>
-            <h3 className={style.text_data}>30,000+</h3>
-            <p className={style.text_data_small}>Happy Customers</p>
+      <main className={style.container}>
+        <div>
+          <h1 className={style.title}>FIND CLOTHES THAT MATCHES YOUR STYLE</h1>
+          <p className={style.text}>
+            Browse through our diverse range of meticulously crafted garments,
+            designed to bring out your individuality and cater to your sense of
+            style.
+          </p>
+          <Link to="/shop">
+            <Button text={"Shop Now"} />
+          </Link>
+
+          <div className={style.data_container}>
+            <div>
+              <h3 className={style.text_data}>200+</h3>
+              <p className={style.text_data_small}>International Brands</p>
+            </div>
+            <div>
+              <h3 className={style.text_data}>2,000+</h3>
+              <p className={style.text_data_small}>High-Quality Products</p>
+            </div>
+            <div>
+              <h3 className={style.text_data}>30,000+</h3>
+              <p className={style.text_data_small}>Happy Customers</p>
+            </div>
           </div>
         </div>
+        <div className={style.image_container}></div>
+      </main>
+      <div className={style.branchs_container}>
+        <p>VERSACE</p>
+        <p>ZARA</p>
+        <p>GUCCI</p>
+        <p>PRADA</p>
+        <p>Calvin Klein</p>
       </div>
-      <div className={style.image_container}></div>
-    </main>
-         <div className={style.branchs_container}>
-         <p>VERSACE</p>
-         <p>ZARA</p>
-         <p>GUCCI</p>
-         <p>PRADA</p>
-         <p>Calvin Klein</p>
-       </div>
-       <section className={style.section}>
-       <h2 className={style.h2}>NEW ARRIVALS</h2>
-       <div className={style.card_container}>
-     {firstNewsProducts.map((item) => (
-               <Link to={"/detail/"+item.product_id}   key={item.product_id} ><Card  name={item.name} price={item.price} urlImage={urlServer+"images/"+item.imageurl}/></Link>
-     ))}
-       </div>
-       <a href="/" className={style.button_center}><ButtonLight text={"View All"}/></a>
-       </section>
 
-       <section className={style.section}>
-       <h2 className={style.h2}>TOP SELLING</h2>
-       <div className={style.card_container}>
-       {firstTopProducts.map((item) => (
-        <Link to={"/detail/"+item.product_id} key={item.product_id}><Card  name={item.name} price={item.price} urlImage={urlServer+"images/"+item.imageurl}/></Link>
+      <CardsSection products={firstNewsProducts} title="NEW ARRIVALS" />
+      <CardsSection products={firstTopProducts} title="TOP SELLING" />
 
-          
-     ))}
-       </div>
-        <a href="/" className={style.button_center}><ButtonLight text={"View All"}/></a>
-    
-       </section>
-
-       <StyleSection/>
-
-       </>
+      <StyleSection />
+    </>
   );
 };
 
